9558478e9bfc48127ca5cd817b33ab3a2ec5d02b
Saruni Karunarathna <sarunikarunarathna9798@gmail.com>
2022-04-29 22:25:41 +0530

    first commit

---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+
+    Route::get('/checkingAuthenticated', function () {
+        return response()->json(['message'=>'You are in', 'status'=>200], 200);
+    });
+
+    Route::post('logout', [AuthController::class, 'logout']);
+});
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
